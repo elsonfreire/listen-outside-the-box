a316584cea8a69016afc8efb09ba92e7b10305f3
elsonfreire <elsonfreirefilho@gmail.com>
2025-11-29 13:40:47 -0300

    Create basic login logic

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,23 +1,59 @@
+import { Navigate } from "react-router-dom";
 import Button from "../components/Button";
+import { useState } from "react";
 
 const LoginPage = () => {
+  const [logged, setLogged] = useState(false);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const users = [
+    {
+      name: "admin",
+      password: "admin",
+    },
+  ];
+
+  const login = () => {
+    const user = users.find((user) => user.name === username);
+
+    if (!user) return console.log("User doesn't exist");
+
+    if (user.password !== password) return console.log("Wrong password");
+
+    setLogged(true);
+    console.log("Logged in");
+  };
+
+  if (logged) return <Navigate to="/" replace />;
+
   return (
     <div>
       <h2 className="font-bold text-2xl">Login</h2>
 
-      <form className="flex flex-col gap-4 my-5">
+      <div className="flex flex-col gap-4 my-5">
         <div>
           <p>Username</p>
-          <input className="bg-neutral-300 rounded-md" />
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="px-2 py-1 bg-neutral-300 rounded-md"
+          />
         </div>
 
         <div>
           <p>Password</p>
-          <input className="bg-neutral-300 rounded-md" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="px-2 py-1 bg-neutral-300 rounded-md"
+          />
         </div>
 
-        <Button type="submit">Login</Button>
-      </form>
+        <Button onClick={login}>Login</Button>
+      </div>
     </div>
   );
 };
