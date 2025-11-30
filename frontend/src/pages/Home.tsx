b5f67982dabb9a20809eac647387575d68cfcf0b
elsonfreire <elsonfreirefilho@gmail.com>
2025-11-30 12:44:18 -0300

    Add suggestion to new song when user likes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,8 @@ import Button from "../components/Button";
 import { useState } from "react";
 
 const HomePage = () => {
+  const [songName, setSongName] = useState("Ela Une Todas as Coisas");
+  const [artistName, setArtistName] = useState("Jorge Vercillo");
   const [songLink, setSongLink] = useState(
     "https://open.spotify.com/track/2fAwUj2Ezq0uB2ClAKEDb1?si=00f82328433e4ade"
   );
@@ -14,17 +16,20 @@ const HomePage = () => {
       .post("http://localhost:5000/analyze", {
         query: "Ela Une Todas as Coisas",
       })
-      .then((response) => console.log(response));
+      .then((response) => response.data.winner)
+      .then((resultSong) => {
+        setSongName(resultSong.title);
+        setArtistName(resultSong.artist);
+        setSongLink(resultSong.link);
+      });
   };
 
   return (
     <div className="flex flex-col gap-10">
       <div className="flex flex-col gap-4 items-center">
         <div>
-          <h2 className="font-bold text-4xl text-center">
-            Ela Une Todas as Coisas
-          </h2>
-          <h3 className="font-bold text-2xl text-center">Jorge Vercílio</h3>
+          <h2 className="font-bold text-4xl text-center">{songName}</h2>
+          <h3 className="font-bold text-2xl text-center">{artistName}</h3>
         </div>
         <a href={songLink} target="_blank" className="text-blue-800 underline">
           Abrir no Spotify
